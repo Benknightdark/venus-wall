40ebc6c95ab5a3b6c37e01a803fda855b77e48ef
ben <moon791017@gmail.com>
2021-07-22 17:29:34 +0800

    aaa

--- api/main.py
+++ api/main.py
@@ -21,7 +21,7 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
-     "http://localhost:3000",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
@@ -31,6 +31,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
@@ -51,7 +52,7 @@ def get_db(request: Request):
         db.close()
 
 
-@app.post("/api/webpage", description="新增網頁類別")
+@app.post("/api/webpage", description="新增WebPage ")
 async def add_web_apge(data: List[view_models.WebPageCreate], db: Session = Depends(get_db)):
     db_item_array = []
     for d in data:
@@ -63,42 +64,43 @@ async def add_web_apge(data: List[view_models.WebPageCreate], db: Session = Depe
     return db_item_array
 
 
-@app.get("/api/webpage", description="取得要抓的所有網頁類別")
+@app.get("/api/webpage", description="取得要抓的所有WebPage ")
 async def get_web_page(db: Session = Depends(get_db)):
     data = db.query(models.WebPage).all()
     return data
 
 
-@app.get("/api/webpage/{id}", description="透過id，取得要抓的網頁類別")
+@app.get("/api/webpage/{id}", description="透過id，取得要抓的WebPage ")
 async def get_web_page_by_id(id: str, db: Session = Depends(get_db)):
     data = db.query(models.WebPage).filter(models.WebPage.ID == id).first()
     return data
 
 
-@app.delete("/api/webpage/{id}", description="透過id，刪除網頁類別")
+@app.delete("/api/webpage/{id}", description="透過id，刪除WebPage ")
 async def get_web_page_by_id(id: str, db: Session = Depends(get_db)):
     data = db.query(models.WebPage).filter(models.WebPage.ID == id).delete()
     db.commit()
     return data
 
 
-@app.put("/api/webpage/{id}", description="透過id，修改網頁類別")
+@app.put("/api/webpage/{id}", description="透過id，修改WebPage ")
 async def get_web_page_by_id(id: str, data: view_models.WebPageCreate, db: Session = Depends(get_db)):
     data = db.query(models.WebPage).filter(
         models.WebPage.ID == id).update(data.dict())
     return data
 
 
-def get_items(id: str, db: Session):
+def update_items(id: str, db: Session):
     web_page = db.query(models.WebPage).filter(
         models.WebPage.ID == id).first()
-    res = httpx.get(web_page.Url)  
-    get_all_page = BeautifulSoup(res.text, "html.parser").find('input',attrs={'name':'custompage'}).next_element['title'].replace('共','').replace('頁','').replace(' ','')
-    logging.info(get_all_page) 
+    res = httpx.get(web_page.Url)
+    get_all_page = BeautifulSoup(res.text, "html.parser").find('input', attrs={
+        'name': 'custompage'}).next_element['title'].replace('共', '').replace('頁', '').replace(' ', '')
+    logging.info(get_all_page)
     web_page_url = web_page.Url.replace('-1.html', '')
     root_page_url = web_page_url
     i: int = 1
-    while i<=int(get_all_page):
+    while i <= int(get_all_page):
         logging.info(f'{i}')
         url = f"{root_page_url}-{i}.html"
         res = httpx.get(url)
@@ -109,41 +111,43 @@ def get_items(id: str, db: Session):
         water_fall = water_fall_root.find_all(
             'div', attrs={'class': 'c cl'})
         logging.info(f'{len(water_fall)}')
-        
+
         for w in water_fall:
-            avator=""
+            avator = ""
             image_name = re.sub('[^\w\-_\. ]', '_', w.a['title'])
             image_url = 'https://www.jkforum.net/'+w.a['href']
             try:
-                if w.a.img==None:
-                    avator= w.a['style'].split("url('")[1][:-3].split("');")[0]
-                elif hasattr(w.a.img,'src'):
-                    avator=w.a.img['src']
+                if w.a.img == None:
+                    avator = w.a['style'].split(
+                        "url('")[1][:-3].split("');")[0]
+                elif hasattr(w.a.img, 'src'):
+                    avator = w.a.img['src']
             except:
-                pass        
+                pass
 
             logging.info(f"{image_name} === {i}")
             logging.info(image_url)
-            logging.info( avator)
+            logging.info(avator)
             selected_item = db.query(models.Item).filter(
                 models.Item.PageName == w.a['href'])
-            item_id=uuid.uuid4()    
+            item_id = uuid.uuid4()
             if selected_item.count() == 1:
                 logging.info('update')
-                item_id=selected_item.first().ID
-                db.query(models.Image).filter(models.Image.ItemID==selected_item.first().ID).delete()
+                item_id = selected_item.first().ID
+                db.query(models.Image).filter(models.Image.ItemID ==
+                                              selected_item.first().ID).delete()
                 selected_item.update(
                     {
-                        "Title":image_name,
-                        "PageName":w.a['href'],
-                        "Url":image_url,
-                        "Avator":avator,
-                        "ModifiedDateTime":datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    })              
+                        "Title": image_name,
+                        "PageName": w.a['href'],
+                        "Url": image_url,
+                        "Avator": avator,
+                        "ModifiedDateTime": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    })
             else:
                 logging.info('insert')
                 db.add(models.Item(ID=item_id, Title=image_name,
-                       PageName=w.a['href'], Url=image_url,WebPageID=id,Avator=avator,
+                       PageName=w.a['href'], Url=image_url, WebPageID=id, Avator=avator,
                        ModifiedDateTime=datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
             image_html = httpx.get(image_url).text
             image_root = BeautifulSoup(image_html, "html.parser")
@@ -151,23 +155,34 @@ def get_items(id: str, db: Session):
             db_images_array = []
             for image in images:
                 try:
-                    db_images_array.append(models.Image(ID=uuid.uuid4(),Url=image.img['file'],ItemID=item_id))
+                    db_images_array.append(models.Image(
+                        ID=uuid.uuid4(), Url=image.img['file'], ItemID=item_id))
                     db.add_all(db_images_array)
                     logging.info(f"  {image.img['file']}")
                 except:
                     pass
-            db.commit()    
+            db.commit()
             logging.info('-------------------------')
         i = i+1
 
 
-@app.post("/api/item/{id}", description="透過網頁類別id，新增或修改此類別底下的item資料")
+@app.post("/api/item/{id}", description="透過WebPage id，新增或修改此類別底下的item資料")
 async def post_item_by_web_page_id(id: str, background_tasks: BackgroundTasks,
-                                  db: Session = Depends(get_db)):
-    background_tasks.add_task(get_items, id, db)
+                                   db: Session = Depends(get_db)):
+    background_tasks.add_task(update_items, id, db)
     return {"message": "開始抓資料"}
-@app.get("/api/item/{id}", description="透過網頁類別id，取得要抓的item資料")
-async def post_item_by_web_page_id(id: str,
+
+
+@app.get("/api/item/{id}", description="透過WebPage id，取得要抓的item資料")
+async def get_item_by_web_page_id(id: str,
                                   db: Session = Depends(get_db)):
-    data=db.query(models.Item).filter(models.Item.WebPageID==id).order_by((models.Item.ModifiedDateTime)).all()
+    data = db.query(models.Item).filter(models.Item.WebPageID ==
+                                        id).order_by((models.Item.ModifiedDateTime)).all()
+    return data
+
+
+@app.get("/api/image/{id}", description="透過item id，取得要抓的image資料")
+async def get_image_by_item_id(id: str,
+                               db: Session = Depends(get_db)):
+    data = db.query(models.Image).filter(models.Image.ItemID == id).all()
     return data
