fb5127c83ab8ef55c0330cc930d0a5ed05dca8d2
ben <moon791017@gmail.com>
2021-08-02 16:58:13 +0800

    OK

--- my-web/src/app/app.component.ts
+++ my-web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-web';
+  display:boolean=true;
+  items: MenuItem[]=[];
+
+    ngOnInit() {
+        this.items = [
+            {label: '首頁', icon: 'pi pi-fw pi-home'},
+            {label: '管理', icon: 'pi pi-fw pi-cog'},
+        ];
+    }
 }
--- my-web/src/app/app.module.ts
+++ my-web/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {SidebarModule} from 'primeng/sidebar';
+import {ButtonModule} from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ToolbarModule} from 'primeng/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SidebarModule,
+    ButtonModule,
+    ToolbarModule,
+    FlexLayoutModule,
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
