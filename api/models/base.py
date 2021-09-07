8834d2a9b5ab716755a9f142462a261b8de6c19d
BenKung <moon791017@gmail.com>
2021-09-07 16:22:18 +0800

    OK

--- api/models/base.py
+++ api/models/base.py
@@ -2,7 +2,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "mssql+pymssql://sa:YourStrong!Passw0rd@localhost:9487/jkf?charset=utf8"
+import os 
+
+SQLALCHEMY_DATABASE_URL =os.environ.get('DB_CONNECT_STRING', "mssql+pymssql://sa:YourStrong!Passw0rd@localhost:9487/jkf?charset=utf8") 
+print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,echo=True
 )
