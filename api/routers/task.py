6ce92cb2bb9758b1b14b1f9f6347754655d98fb5
ben <moon791017@gmail.com>
2021-09-13 09:38:12 +0800

    ok

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,14 +1,7 @@
 import httpx
-from sqlalchemy.sql.expression import and_, asc, desc
-from sqlalchemy.sql.functions import func
-from starlette.background import BackgroundTasks
 from dependencies import get_db, send_task,task_info,all_task_info
 from fastapi.params import Depends
-from typing import Optional
 from fastapi import APIRouter
-from sqlalchemy.orm import Session
-from models import models
-import json
 
 router = APIRouter()
 
@@ -20,11 +13,20 @@ async def get_item_by_web_page_id(id: str,
     req = client.get(f'{flower_task_info}/{id}')
     res = req.json()
     return res   
-@router.get("/tasks", summary="取得所有task")
+@router.get("/tasks", summary="取得所有爬蟲任務資訊")
 async def get_item_by_web_page_id(
                                   flower_all_task_info: str = Depends(all_task_info)):
     transport = httpx.HTTPTransport(retries=5)
     client = httpx.Client(transport=transport)                              
     req = client.get(f'{flower_all_task_info}')
     res = req.json()
-    return res                                   
+    return res 
+
+@router.get("/tasks/results/{id}", summary="取得特定webpage id的所有爬蟲執行結果")
+async def get_item_by_web_page_id(
+                                  flower_all_task_info: str = Depends(all_task_info)):
+    transport = httpx.HTTPTransport(retries=5)
+    client = httpx.Client(transport=transport)                              
+    req = client.get(f'{flower_all_task_info}')
+    res = req.json()
+    return res 
