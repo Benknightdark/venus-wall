54a9131c930b3c0692af347ffd1a728098896582
ben <moon791017@gmail.com>
2021-09-13 16:03:19 +0800

    add TaskResultsForm

--- WebSite/src/app/services/task.service.ts
+++ WebSite/src/app/services/task.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, retry, share } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
-import { TaskInfo } from '../models/data.model';
+import { TaskInfo, TaskResult } from '../models/data.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,28 +12,37 @@ export class TaskService {
   private _currentTaskStatusList$: Subject<{ [webPageId: string]: TaskInfo }> = new Subject<{ [webPageId: string]: TaskInfo }>();
   readonly currentTaskStatusList$ = this._currentTaskStatusList$.asObservable();
   private curretnTaskStatusList: { [webPageId: string]: TaskInfo } = {};
-
+  private _taskResultsList$: Subject<TaskResult[]> = new Subject<TaskResult[]>();
+  readonly taskResultsList$ = this._taskResultsList$.asObservable();
   constructor(private http: HttpClient) { }
 
+  getTaskInfo(webPageId: string | undefined) {
+    this.http.get<TaskResult[]>(`${environment.apiUrl}/api/task/results/${webPageId}`).pipe(
+      share(),
+      debounceTime(300),
+      distinctUntilChanged(), retry(5)).subscribe(res => {
+        this._taskResultsList$.next(res)
+      })
+  }
   getCurrentTaskStatus(webPageId?: string, taskId?: string) {
     if (webPageId == null) {
       this._currentTaskStatusList$.next(this.curretnTaskStatusList);
       this.http.get<{ [uuid: string]: TaskInfo }>(`${environment.apiUrl}/api/tasks`).pipe(
         share(),
-       debounceTime(300),
-      distinctUntilChanged(),retry(5))
-      .subscribe(r => {
-        for (const item of Object.keys(this.curretnTaskStatusList)) {
-          this.curretnTaskStatusList[item]=r[ this.curretnTaskStatusList[item].uuid!]
-        }
-        this._currentTaskStatusList$.next(this.curretnTaskStatusList);
-      })
+        debounceTime(300),
+        distinctUntilChanged(), retry(5))
+        .subscribe(r => {
+          for (const item of Object.keys(this.curretnTaskStatusList)) {
+            this.curretnTaskStatusList[item] = r[this.curretnTaskStatusList[item].uuid!]
+          }
+          this._currentTaskStatusList$.next(this.curretnTaskStatusList);
+        })
     } else {
       taskId = (taskId === undefined || taskId === null) ? this.curretnTaskStatusList[String(webPageId).toUpperCase()].uuid : taskId;
       this.http.get<TaskInfo>(`${environment.apiUrl}/api/task/${taskId}`).pipe(
         share(),
-       debounceTime(300),
-      distinctUntilChanged(),retry(5))
+        debounceTime(300),
+        distinctUntilChanged(), retry(5))
         .subscribe(r => {
           this.curretnTaskStatusList[String(webPageId).toUpperCase()] = r;
           this._currentTaskStatusList$.next(this.curretnTaskStatusList);
