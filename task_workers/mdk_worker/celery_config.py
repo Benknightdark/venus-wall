ab63d2f051b57356ff1d1274b8e02811b5bb1545
ben <moon791017@gmail.com>
2021-09-15 12:00:47 +0800

    qqq

--- task_workers/mdk_worker/celery_config.py
+++ task_workers/mdk_worker/celery_config.py
@@ -1,4 +1,5 @@
 from os import environ
+from kombu import Exchange, Queue
 
 broker_url = environ.get(
     'BROKER_URL', 'pyamqp://rabbitmq:rabbitmq@localhost:5672//')
@@ -10,3 +11,13 @@ timezone = 'Asia/Taipei'
 task_serializer = 'json'
 worker_send_task_events = True
 result_persistent = True
+
+
+# task_exchange = Exchange('mdk_worker', type='direct')
+# task_create_missing_queues = False
+# task_queues = [
+#     Queue('mdk_worker', task_exchange, routing_key='mdk_worker'),
+# ]
+# task_routes = {
+#     'mdk_worker.update_item': {'queue': 'mdk_worker', 'routing_key': 'mdk_worker'}
+# }
