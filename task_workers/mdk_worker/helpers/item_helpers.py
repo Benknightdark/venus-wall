1b11191a1df0be703247f88a364a6268fc132dc3
ben <moon791017@gmail.com>
2021-09-16 15:53:24 +0800

    update mdk item

--- task_workers/mdk_worker/helpers/item_helpers.py
+++ task_workers/mdk_worker/helpers/item_helpers.py
@@ -45,67 +45,81 @@ class ItemHandler:
                 logging.info(f'{i}')
                 url = f"{web_page.Url}&filter=&orderby=lastpost&page={i}"
                 res = httpx.get(url)
+                logging.info(url)
                 root = BeautifulSoup(res.text, "html.parser")
                 lists = root.find_all(
                     'div', attrs={'class': 'nex_waterfallbox'})
-                for l in lists:
-                    href = l.find('a')
-                    title = href['title']
-                    page_name = href['href']
-                    seq=page_name.split('&')[1].replace('tid=','')
-                    link = f"https://www.mdkforum.com/{href['href']}"
-                    avator = ''
-                    logging.info(href.img)
-                    if href.img != None:
-                        avator = f"https://www.mdkforum.com/{href.img['src']}"
-
-                    modfied_date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                    logging.info(f"{href['title']} ==== {i} - {get_all_page}")
-                    logging.info('----------------------')
-                    selected_item = db.query(models.Item).filter(
-                        models.Item.PageName == page_name)
-                    item_id = uuid.uuid4()
-                    if selected_item.count() == 1:
-                        item_id = selected_item.first().ID
-                        db.query(models.Image).filter(models.Image.ItemID ==
-                                                      selected_item.first().ID).delete()
-                        selected_item.update(
-                            {
-                                "Title": title,
-                                "PageName": page_name,
-                                "Page": i,
-                                "Seq":int(seq),
-                                "Url": link,
-                                "Avator": avator,
-                                "Enable": True,
-                                "ModifiedDateTime": modfied_date_time
-                            })
-                    else:
-                        logging.info('insert')
-                        add_data = models.Item(ID=item_id, Title=title, Page=i, Enable=True,Seq=int(seq),
-                                               PageName=page_name, Url=link, WebPageID=id, Avator=avator,
-                                               ModifiedDateTime=modfied_date_time)
-                        db.add(add_data)
-                    content_res = httpx.get(
-                        f"https://www.mdkforum.com/{href['href']}")
-                    root_content = BeautifulSoup(
-                        content_res.text, "html.parser")
-                    content_image = root_content.find_all('ignore_js_op')
-                    db_images_array = []
-                    for c in content_image:
+                if lists == []:
+                    lists = root.find_all(
+                        'th', attrs={'class': 'new forumtit'})
+                if len(lists) > 0:
+                    for l in lists:
+                        href = l.find('a')
                         try:
-                            logging.info(
-                                c.find('img', attrs={'class': 'zoom'})['file'])
-                            image_url = c.find(
-                                'img', attrs={'class': 'zoom'})['file']
-                            db_images_array.append(models.Image(
-                                ID=uuid.uuid4(), Url=f"https://www.mdkforum.com/{image_url}", ItemID=item_id))
+                            title = href['title']
                         except:
-                            pass
-                    if len(db_images_array) > 1:
-                        db.add_all(db_images_array)
-                    db.commit()
-                    logging.info('-------------------------')
+                            title = href.text
+                        page_name = href['href']
+                        seq = page_name.split('&')[1].replace('tid=', '')
+                        link = f"https://www.mdkforum.com/{href['href']}"
+                        avator = ''
+                        try:
+                            avator = f"https://www.mdkforum.com/{href.img['src']}"
+                        except:
+                            forumlist_pics = l.find(
+                                'div', attrs={'class': 'nex_forumlist_pics'}).find('a')
+                            logging.info(forumlist_pics)
+                            if forumlist_pics != None:
+                                avator = f"https://www.mdkforum.com/{forumlist_pics.img['src']}"
+                        modfied_date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                        logging.info(
+                            f"{title} ==== {i} - {get_all_page}")
+                        logging.info('----------------------')
+                        selected_item = db.query(models.Item).filter(
+                            models.Item.PageName == page_name)
+                        item_id = uuid.uuid4()
+                        if selected_item.count() == 1:
+                            item_id = selected_item.first().ID
+                            db.query(models.Image).filter(models.Image.ItemID ==
+                                                          selected_item.first().ID).delete()
+                            selected_item.update(
+                                {
+                                    "Title": title,
+                                    "PageName": page_name,
+                                    "Page": i,
+                                    "Seq": int(seq),
+                                    "Url": link,
+                                    "Avator": avator,
+                                    "Enable": True,
+                                    "ModifiedDateTime": modfied_date_time
+                                })
+                        else:
+                            logging.info('insert')
+                            add_data = models.Item(ID=item_id, Title=title, Page=i, Enable=True, Seq=int(seq),
+                                                   PageName=page_name, Url=link, WebPageID=id, Avator=avator,
+                                                   ModifiedDateTime=modfied_date_time)
+                            db.add(add_data)
+                        content_res = httpx.get(
+                            f"https://www.mdkforum.com/{href['href']}")
+                        root_content = BeautifulSoup(
+                            content_res.text, "html.parser")
+                        content_image = root_content.find_all('ignore_js_op')
+                        db_images_array = []
+                        for c in content_image:
+                            try:
+                                logging.info(
+                                    c.find('img', attrs={'class': 'zoom'})['file'])
+                                image_url = c.find(
+                                    'img', attrs={'class': 'zoom'})['file']
+                                db_images_array.append(models.Image(
+                                    ID=uuid.uuid4(), Url=f"https://www.mdkforum.com/{image_url}", ItemID=item_id))
+                            except:
+                                pass
+                        if len(db_images_array) > 1:
+                            db.add_all(db_images_array)
+                        db.commit()
+                        logging.info('-------------------------')
+
                 i = i+1
             return {"status": f"{web_page.Name} - {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"}
         except Exception as e:
