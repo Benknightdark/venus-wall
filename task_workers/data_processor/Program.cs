2463389ed13a3eed03f155f6646101a0453bfe70
Ben <moon791017@gmail.com>
2021-11-17 12:32:57 +0800

    test

--- task_workers/data_processor/Program.cs
+++ task_workers/data_processor/Program.cs
@@ -25,7 +25,7 @@ app.MapGet("/dapr/subscribe", () =>
 });
 
 // 更新jkf論壇資料
-app.MapPost("/process-jkf", (BeautyDBContext db, MessageQueueModel MessageData) =>
+app.MapPost("/process-jkf", async ( MessageQueueModel MessageData) =>
 {
     var Data = System.Text.Json.JsonSerializer.Serialize(MessageData.data);
     var ItemData = JsonConvert.DeserializeObject<Item>(Data);
@@ -35,9 +35,40 @@ app.MapPost("/process-jkf", (BeautyDBContext db, MessageQueueModel MessageData) 
     app.Logger.LogInformation(ItemData.WebPageID.ToString());
     app.Logger.LogInformation(ItemData.ID.ToString());
     app.Logger.LogInformation(ItemData.ModifiedDateTime.ToString());
+    using (var scope = app.Services.CreateScope())
+    {
+        var db= scope.ServiceProvider.GetRequiredService<BeautyDBContext>();
+        
+        app.Logger.LogInformation(db.Items.Count().ToString());
 
+    }
+   // var CurrentItme=db.Items.Where(a=>a.Title==ItemData.Title);
+    // app.Logger.LogInformation((await CurrentItme.AnyAsync()).ToString());
+    //if (CurrentItme.Any()){
+       // var UpdateCurrentItme=await CurrentItme.FirstAsync();
+        //app.Logger.LogInformation(UpdateCurrentItme.Title);
+        // UpdateCurrentItme.Title=ItemData.Title;
+        // UpdateCurrentItme.PageName=ItemData.PageName;
+        // UpdateCurrentItme.Url=ItemData.Url;
+        // UpdateCurrentItme.Avator=ItemData.Avator;
+        // UpdateCurrentItme.ModifiedDateTime=ItemData.ModifiedDateTime;
+        // UpdateCurrentItme.Page=ItemData.Page;
+        // UpdateCurrentItme.Seq=ItemData.Seq;
+        // db.Update(UpdateCurrentItme);
+    //}
+    // else{
+    //     await db.AddAsync(ItemData);
+    // }
+
+    // if(ItemData.Images.Count()>0){
+    //         var CurrentImages=db.Images.Where(a=>a.ItemID==ItemData.ID);
+    //         if (await CurrentImages.AnyAsync())
+    //             db.RemoveRange(CurrentImages);  
+    //         await db.AddAsync(ItemData.Images);       
+    // }
+    // await db.SaveChangesAsync();
     app.Logger.LogInformation("=============");
 
-    return global::System.Threading.Tasks.Task.FromResult(Data);
+    return (Data);
 });
 app.Run();
