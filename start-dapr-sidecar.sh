a6cf7feae496c101acc4dfcdea2932746481cf37
Ben <moon791017@gmail.com>
2021-12-01 08:49:01 +0800

    update start-dapr-sidecar.sh

--- start-dapr-sidecar.sh
+++ start-dapr-sidecar.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
-#
-# Open new iTerm window from the command line
-#
-# Usage:
-#     iterm                   Opens the current directory in a new iTerm window
-#     iterm [PATH]            Open PATH in a new iTerm window
-#     iterm [CMD]             Open a new iTerm window and execute CMD
-#     iterm [PATH] [CMD] ...  You can prob'ly guess
-#
-# Example:
-#     iterm ~/Code/HelloWorld ./setup.sh
-#
-# References:
-#     iTerm AppleScript Examples:
-#     https://gitlab.com/gnachman/iterm2/wikis/Applescript
-# 
-# Credit:
-#     Inspired by tab.bash by @bobthecow
-#     link: https://gist.github.com/bobthecow/757788
-#
 
-# OSX only
 [ `uname -s` != "Darwin" ] && return
+# 清除所有daprd執行緒
+ps aux | grep daprd|grep -v grep | awk '{print $2}'|xargs kill -9
+# 取出當前的目錄
 PWD="${PWD}"
 
 function iterm () {
     osascript &>/dev/null <<EOF
 tell application "iTerm"
-	--Create initial window
-	--create window with default profile
-	
-	--Create a second tab in the initial window
 	tell current window
+		create tab with default profile
+		create tab with default profile
+		create tab with default profile
+		create tab with default profile
 		create tab with default profile
 	end tell
 	
-	--Send a command to the first tab
-	tell current session of tab 1 of current window
+	tell current session of tab 1 of current window	
 		write text "cd $PWD"
+		write text "daprd --app-id 'api-service' --app-port '8780' --components-path './components' --dapr-grpc-port '53317' --dapr-http-port '3500' --metrics-port '9090'"
 	end tell
 	
-	--Send a couple of commands to the second tab
 	tell current session of tab 2 of current window
 		write text "cd $PWD"
+		write text "daprd --app-id 'jkf-worker' --app-port '8789' --components-path './components' --dapr-grpc-port '53318' --dapr-http-port '3501' --metrics-port '9091'"
 	end tell
 
-	--Select the first tab
+	tell current session of tab 3 of current window
+		write text "cd $PWD"
+		write text "daprd --app-id 'mdk-worker' --app-port '8790' --components-path './components' --dapr-grpc-port '53319' --dapr-http-port '3502' --metrics-port '9092'"
+	end tell
+
+	tell current session of tab 4 of current window
+		write text "cd $PWD"
+		write text "daprd --app-id 'data-processor' --app-port '5235' --components-path './components' --dapr-grpc-port '53320' --dapr-http-port '3503' --metrics-port '9093'"
+	end tell
+
+	tell current session of tab 5 of current window
+		write text "cd $PWD"
+		write text "daprd --app-id 'jkf-crawler' --app-port '8800' --components-path './components' --dapr-grpc-port '53321' --dapr-http-port '3504' --metrics-port '9094'"
+	end tell
+
+	tell current session of tab 6 of current window
+		write text "cd $PWD"
+		write text "daprd --app-id 'mdk-crawler' --app-port '8791' --components-path './components' --dapr-grpc-port '53322' --dapr-http-port '3505' --metrics-port '9095'"
+	end tell
+
 	tell tab 1 of current window
 		select
 	end tell
