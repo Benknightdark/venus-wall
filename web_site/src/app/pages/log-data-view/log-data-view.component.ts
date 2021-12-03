bfe21cce9316bf7854d3ed94cd6d575420316a4e
ben <moon791017@gmail.com>
2021-12-03 11:53:12 +0800

    update log data service and view component

--- web_site/src/app/pages/log-data-view/log-data-view.component.ts
+++ web_site/src/app/pages/log-data-view/log-data-view.component.ts
@@ -1,5 +1,5 @@
 import { LogService } from './../../services/log.service';
-import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { NzTableComponent } from 'ng-zorro-antd/table';
 
@@ -8,24 +8,30 @@ import { NzTableComponent } from 'ng-zorro-antd/table';
   templateUrl: './log-data-view.component.html',
   styleUrls: ['./log-data-view.component.css']
 })
-export class LogDataViewComponent implements OnInit, AfterViewInit {
+export class LogDataViewComponent implements OnInit, OnDestroy, AfterViewInit {
   $getData!: Subscription;
   constructor(private lgoService: LogService) { }
+
   @Input() data: any;
-  tableData: any[]=[];
+  tableData: any[] = [];
   loading: boolean = true;
-  currentDataCount:number=0;
-  offset=0;
-  limit=20
+  currentDataCount: number = 0;
+  offset = 0;
+  limit = 20;
+  stopGetMoreData: boolean = false;
   @ViewChild('basicTable', { static: false }) nzTableComponent?: NzTableComponent<any>;
 
   ngOnInit(): void {
-this.loadData();
+    this.loadData();
   }
-  loadData(){
-    this.$getData = this.lgoService.getLogData(this.data['name'],this.offset,this.limit).subscribe(logData => {
-      this.tableData =[...this.tableData,...logData];
-      this.currentDataCount=this.tableData.length-1
+  loadData() {
+    this.$getData = this.lgoService.getLogData(this.data['name'], this.offset, this.limit).subscribe(logData => {
+      if (logData.length > 0) {
+        this.tableData = [...this.tableData, ...logData];
+        this.currentDataCount = this.tableData.length
+      } else {
+        this.stopGetMoreData = true;
+      }
       this.loading = false;
     });
   }
@@ -33,19 +39,32 @@ this.loadData();
     return data.index;
   }
   ngAfterViewInit(): void {
-    this.nzTableComponent?.cdkVirtualScrollViewport?.scrolledIndexChange.subscribe((scrolledIndex: number) => {
-      if(scrolledIndex===this.currentDataCount){
-        console.log("Got YOU")
-        this.offset=  this.offset+this.limit;
-        this.loading=true;
+    this.nzTableComponent?.cdkVirtualScrollViewport?.scrolledIndexChange.subscribe((scrollIndex: number) => {
+      if (this.stopGetMoreData)
+        return;
+      if (scrollIndex === 0)
+        return;
+      if (this.loading)
+        return;
+      const end = this.nzTableComponent?.cdkVirtualScrollViewport?.getRenderedRange().end;
+      const total = this.nzTableComponent?.cdkVirtualScrollViewport?.getDataLength();
+      console.log(end, total)
+      if (end === total) {
+        this.offset = this.offset + this.limit;
+        this.loading = true;
         this.loadData();
       }
     });
   }
-  nextBatch(index: any) {
-    console.log(index)
+  resetVariable() {
+    this.currentDataCount = 0;
+    this.offset = 0;
+    this.limit = 20;
+    this.stopGetMoreData = false;
   }
-  OnDestroy(): void {
+  ngOnDestroy(): void {
     this.$getData.unsubscribe();
+    this.resetVariable();
+    console.log('destroy')
   }
 }
--- web_site/src/app/services/log.service.ts
+++ web_site/src/app/services/log.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { debounceTime, distinctUntilChanged, share } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -9,7 +10,10 @@ export class LogService {
 
   constructor(private http: HttpClient) { }
   getLogData(name:string,offset: number = 0, limit: number = 20) {
-    return this.http.get<[]>(`${environment.apiUrl}/api/log/${name}?offset=${offset}&limit=${limit}`)
+    return this.http.get<[]>(`${environment.apiUrl}/api/log/${name}?offset=${offset}&limit=${limit}`).pipe(
+      share(),
+      debounceTime(300),
+      distinctUntilChanged())
   }
 
 }
