04c28a26afbf107e7f661844652afbb120c0e215
ben <moon791017@gmail.com>
2022-05-31 11:12:35 +0800

    OK

--- web_site_v2/components/loading.tsx
+++ web_site_v2/components/loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
     return (
         <div
-            className="fixed  inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50"
+            className="fixed  inset-0 bg-gray-100 bg-opacity-20 overflow-y-auto h-full w-full z-50"
         >
             <div className="flex flex-col items-center justify-center">
             <progress className="progress w-screen"></progress>
