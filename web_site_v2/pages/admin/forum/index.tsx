cd5acdf3bd46492ffe99a7b6278a98933f03ede6
ben <moon791017@gmail.com>
2022-06-01 17:33:49 +0800

    aa

--- web_site_v2/pages/admin/forum/index.tsx
+++ web_site_v2/pages/admin/forum/index.tsx
@@ -8,8 +8,37 @@ const Index = () => {
         { fallbackData: defaultAdminGlobalStoreData })
     adminGlobalStoreMutate({ ...defaultAdminGlobalStoreData, pageTitle: '論壇管理', pageDescription: '管理要爬的論壇網站' }, false)
     return (
-        <div>
-            Enter
+        <div className="overflow-x-auto ">
+            <table className="table w-full ">
+                <thead className=''>
+                    <tr>
+                        <th className='bg-green-200'></th>
+                        <th className='bg-green-200'>Name</th>
+                        <th className='bg-green-200'>Job</th>
+                        <th className='bg-green-200'>Favorite Color</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="hover">
+                        <th>1</th>
+                        <td>Cy Ganderton</td>
+                        <td>Quality Control Specialist</td>
+                        <td>Blue</td>
+                    </tr>
+                    <tr className="hover">
+                        <th>2</th>
+                        <td>Hart Hagerty</td>
+                        <td>Desktop Support Technician</td>
+                        <td>Purple</td>
+                    </tr>
+                    <tr className="hover">
+                        <th>3</th>
+                        <td>Brice Swyre</td>
+                        <td>Tax Accountant</td>
+                        <td>Red</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
