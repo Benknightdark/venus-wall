cf3c7fc5a457137110efc52afed81e4d68860ef9
ben <moon791017@gmail.com>
2022-06-02 11:02:42 +0800

    aa

--- web_site_v2/pages/admin/forum/index.tsx
+++ web_site_v2/pages/admin/forum/index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
     const defaultPageList = [1, 2, 3, 4, 5]
     const [pageList, setPageList] = useState(defaultPageList)
     const [page, setPage] = useState(1)
-    const { data: forumData, mutate: forumMutate, error: forumError } = useSWR(`${process.env.NEXT_PUBLIC_APIURL}/api/forum-table?offset=${page-1}&limit=10`,
+    const { data: forumData, mutate: forumMutate, error: forumError } = useSWR(`${process.env.NEXT_PUBLIC_APIURL}/api/forum-table?offset=${page - 1}&limit=10`,
         fetcher)
     const changePage = async (curretnPage: number) => {
         console.log(curretnPage)
@@ -25,16 +25,16 @@ const Index = () => {
             setPageList(defaultPageList)
         }
         await forumMutate()
-        setTimeout(() => {
-            document.getElementById('contentBody')!.scrollTo(0, 9999);
-        }, 200)
+        // setTimeout(() => {
+        //     document.getElementById('contentBody')!.scrollTo(0, 9999);
+        // }, 200)
     }
     if (!forumData) return <Loading></Loading>
     if (forumError) return <Loading></Loading>
     return (
-        <div className="overflow-x-auto ">
+        <div className="flex flex-col">
             <div className="flex p-4 text-sm text-gray-700 bg-orange-100 rounded-lg  
-                                dark:text-gray-300 justify-between" role="alert">
+                                 justify-between"  role="alert">
                 <span className="text-2xl">論壇管理</span>
                 <div className="tooltip tooltip-left" data-tip="重新取得資料">
                     <IoIosRefreshCircle className="inline flex-shrink-0 mr-3 w-8 h-8 cursor-pointer hover:h-10 hover:w-10" onClick={() => {
@@ -44,48 +44,51 @@ const Index = () => {
                     </IoIosRefreshCircle>
                 </div>
             </div>
-            <table className="table w-full ">
-                <thead className=''>
-                    <tr>
-                        <th className='bg-green-200'></th>
-                        <th className='bg-green-200'>Name</th>
-                        <th className='bg-green-200'>CreatedTime</th>
-                        <th className='bg-green-200'>WorkerName</th>
-                        <th className='bg-green-200'>Enable</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        forumData && forumData.data.map((f: any) => <tr key={f.key}>
-                            <th>{ }</th>
-                            <th>{f.Name}</th>
-                            <th>{f.CreatedTime}</th>
-                            <th>{f.WorkerName}</th>
-                            <th>{f.Enable.toString()}</th>
-                        </tr>)
-                    }
+            <div className="overflow-x-auto ">
+                <table className="table w-full ">
+                    <thead className=''>
+                        <tr>
+                            <th className='bg-green-200'></th>
+                            <th className='bg-green-200'>Name</th>
+                            <th className='bg-green-200'>CreatedTime</th>
+                            <th className='bg-green-200'>WorkerName</th>
+                            <th className='bg-green-200'>Enable</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            forumData && forumData.data.map((f: any) => <tr key={f.key}>
+                                <th>{ }</th>
+                                <th>{f.Name}</th>
+                                <th>{f.CreatedTime}</th>
+                                <th>{f.WorkerName}</th>
+                                <th>{f.Enable.toString()}</th>
+                            </tr>)
+                        }
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
 
+            </div>
             {
                 forumData && <div className="flex justify-center btn-group">
                     <button className={`btn ${page === 1 ? 'btn-info' : ''}`} onClick={() =>
-                         changePage(1)
-                         }>1</button>
+                        changePage(1)
+                    }>1</button>
                     {page >= 5 && <button className="btn btn-disabled">...</button>}
                     {
                         pageList.filter(a => a !== 1 && a < forumData['totalDataCount']).map(a => <button key={a} className={`btn ${page === a ? 'btn-info' : ''}`}
                             onClick={() => changePage(a)
-                        }>{a}</button>)
+                            }>{a}</button>)
                     }
                     {forumData['totalDataCount'] !== page && <button className="btn btn-disabled">...</button>}
                     <button className={`btn ${page === forumData['totalDataCount'] ? 'btn-info' : ''}`} onClick={() =>
                         changePage(forumData['totalDataCount'])
-                        }>{forumData['totalDataCount']}</button>
+                    }>{forumData['totalDataCount']}</button>
                 </div>
             }
         </div>
+
     );
 }
 Index.getLayout = function getLayout(page: ReactElement) {
