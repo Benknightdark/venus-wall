208b07761937eddf4cff788df0443f88a12d9928
ben <moon791017@gmail.com>
2022-06-02 10:34:09 +0800

    update

--- web_site_v2/pages/admin/forum/index.tsx
+++ web_site_v2/pages/admin/forum/index.tsx
@@ -1,44 +1,90 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import useSWR from "swr";
+import Loading from "../../../components/loading";
 import { adminGlobalStore, defaultAdminGlobalStoreData } from "../../../stores/admon-global-store";
 import AdminLayout from "../../utils/admin-layout";
+import { IoIosRefreshCircle } from 'react-icons/io'
+
+const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 const Index = () => {
     const { data: adminGlobalStoreData, mutate: adminGlobalStoreMutate } = useSWR(adminGlobalStore,
         { fallbackData: defaultAdminGlobalStoreData })
     adminGlobalStoreMutate({ ...defaultAdminGlobalStoreData, pageTitle: '論壇管理', pageDescription: '管理要爬的論壇網站' }, false)
+    const defaultPageList = [1, 2, 3, 4, 5]
+    const [pageList, setPageList] = useState(defaultPageList)
+    const [page, setPage] = useState(1)
+    const { data: forumData, mutate: forumMutate, error: forumError } = useSWR(`${process.env.NEXT_PUBLIC_APIURL}/api/forum-table?offset=${page-1}&limit=10`,
+        fetcher)
+    const changePage = async (curretnPage: number) => {
+        console.log(curretnPage)
+        await setPage(curretnPage)
+        if (curretnPage >= 5) {
+            setPageList([curretnPage - 2, curretnPage - 1, curretnPage, curretnPage + 1, curretnPage + 2])
+        } else {
+            setPageList(defaultPageList)
+        }
+        await forumMutate()
+        setTimeout(() => {
+            document.getElementById('contentBody')!.scrollTo(0, 9999);
+        }, 200)
+    }
+    if (!forumData) return <Loading></Loading>
+    if (forumError) return <Loading></Loading>
     return (
         <div className="overflow-x-auto ">
+            <div className="flex p-4 text-sm text-gray-700 bg-orange-100 rounded-lg  
+                                dark:text-gray-300 justify-between" role="alert">
+                <span className="text-2xl">論壇管理</span>
+                <div className="tooltip tooltip-left" data-tip="重新取得資料">
+                    <IoIosRefreshCircle className="inline flex-shrink-0 mr-3 w-8 h-8 cursor-pointer hover:h-10 hover:w-10" onClick={() => {
+                        setPage(1);
+                        forumMutate();
+                    }}>
+                    </IoIosRefreshCircle>
+                </div>
+            </div>
             <table className="table w-full ">
                 <thead className=''>
                     <tr>
                         <th className='bg-green-200'></th>
                         <th className='bg-green-200'>Name</th>
-                        <th className='bg-green-200'>Job</th>
-                        <th className='bg-green-200'>Favorite Color</th>
+                        <th className='bg-green-200'>CreatedTime</th>
+                        <th className='bg-green-200'>WorkerName</th>
+                        <th className='bg-green-200'>Enable</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="hover">
-                        <th>1</th>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
-                    <tr className="hover">
-                        <th>2</th>
-                        <td>Hart Hagerty</td>
-                        <td>Desktop Support Technician</td>
-                        <td>Purple</td>
-                    </tr>
-                    <tr className="hover">
-                        <th>3</th>
-                        <td>Brice Swyre</td>
-                        <td>Tax Accountant</td>
-                        <td>Red</td>
-                    </tr>
+                    {
+                        forumData && forumData.data.map((f: any) => <tr key={f.key}>
+                            <th>{ }</th>
+                            <th>{f.Name}</th>
+                            <th>{f.CreatedTime}</th>
+                            <th>{f.WorkerName}</th>
+                            <th>{f.Enable.toString()}</th>
+                        </tr>)
+                    }
+
                 </tbody>
             </table>
+
+            {
+                forumData && <div className="flex justify-center btn-group">
+                    <button className={`btn ${page === 1 ? 'btn-info' : ''}`} onClick={() =>
+                         changePage(1)
+                         }>1</button>
+                    {page >= 5 && <button className="btn btn-disabled">...</button>}
+                    {
+                        pageList.filter(a => a !== 1 && a < forumData['totalDataCount']).map(a => <button key={a} className={`btn ${page === a ? 'btn-info' : ''}`}
+                            onClick={() => changePage(a)
+                        }>{a}</button>)
+                    }
+                    {forumData['totalDataCount'] !== page && <button className="btn btn-disabled">...</button>}
+                    <button className={`btn ${page === forumData['totalDataCount'] ? 'btn-info' : ''}`} onClick={() =>
+                        changePage(forumData['totalDataCount'])
+                        }>{forumData['totalDataCount']}</button>
+                </div>
+            }
         </div>
     );
 }
