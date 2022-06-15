750a53655b761569067bc4a36db3260e0332a938
ben <moon791017@gmail.com>
2022-06-15 17:18:24 +0800

    OK

--- web_site_v2/pages/admin/forum/detail/[id].tsx
+++ web_site_v2/pages/admin/forum/detail/[id].tsx
@@ -5,6 +5,7 @@ import useSWR from "swr";
 import Loading from "../../../../components/loading";
 import { adminGlobalStore, defaultAdminGlobalStoreData } from "../../../../stores/admon-global-store";
 import AdminLayout from "../../../utils/admin-layout";
+import {GoGlobe} from "react-icons/go"
 const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 const Detail = () => {
@@ -26,8 +27,8 @@ const Detail = () => {
     return (
         <div className="bg-white shadow overflow-hidden sm:rounded-lg">
             <div className="px-4 py-5 sm:px-6">
-                <h3 className="text-lg leading-6 font-medium text-gray-900">{forumData[0]['Name']}</h3>
-                <p className="mt-1 max-w-2xl text-sm text-gray-500">建立時間：{forumData[0]['CreatedTime']}</p>
+               { forumData&&<h3 className="text-lg leading-6 font-medium text-gray-900">{forumData[0]['Name']}</h3> }
+               { forumData&&<p className="mt-1 max-w-2xl text-sm text-gray-500">建立時間：{forumData[0]['CreatedTime']}</p>}
             </div>
             <div className="border-t border-gray-200">
                 <dl>
@@ -41,7 +42,11 @@ const Detail = () => {
                     </div>
                     <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt className="text-sm font-medium text-gray-500">Enable</dt>
-                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{forumData[0]['Enable'].toString()}</dd>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                           
+                            <input type="checkbox" className="toggle" disabled checked={forumData[0]['Enable']} />
+
+                            </dd>
                     </div>
                     <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt className="text-sm font-medium text-gray-500">WebPages</dt>
@@ -52,11 +57,14 @@ const Detail = () => {
                                         key={w.ID}
                                         className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
                                         <div className="w-0 flex-1 flex items-center">
-                                            <svg className="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            {/* <svg className="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                 <path fillRule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clipRule="evenodd" />
-                                            </svg>
+                                            </svg> */}
+                                            <GoGlobe className="flex-shrink-0 h-5 w-5 text-gray-400"></GoGlobe>
+
                                             <span className="ml-2 flex-1 w-0 truncate"> {w.Name} </span>
                                         </div>
+
                                         <div className="ml-4 flex-shrink-0">
                                             <Link href={`/board/${w.ID}`} 
                                             className="font-medium text-indigo-600 hover:text-indigo-500">
