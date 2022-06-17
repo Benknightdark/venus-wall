c0fa51a8fdb175f3e93d33795e205f3f0469a77b
ben <moon791017@gmail.com>
2022-06-17 13:37:44 +0800

    OK

--- web_site_v2/pages/admin/forum/edit/[id].tsx
+++ web_site_v2/pages/admin/forum/edit/[id].tsx
@@ -8,6 +8,8 @@ import AdminLayout from "../../../utils/admin-layout";
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
+import { FaPlusCircle } from "react-icons/fa";
+import { FiTrash2 } from "react-icons/fi";
 const schema = yup.object({
     name: yup.string().required(),
     workerName: yup.number().required(),
@@ -31,7 +33,7 @@ const Edit = () => {
     if (webPageError && forumError) return <Loading></Loading>
     return (
         <div>
-            <div className="mt-8 ">
+            <div className="mt-2 ">
                 <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 gap-3">
                     <label className="block">
                         <span className="text-gray-700">Name</span>
@@ -63,9 +65,49 @@ const Edit = () => {
                         input input-bordered input-primary w-full" {...register("createdTime")} />
                         <p className="text-red-500">{errors.createdTime?.message}</p>
                     </label>
-                   <div className="flex flex-row items-center  justify-center space-x-2">
-                   <button type="submit" className="btn btn-success">送出</button>
-                   </div>
+
+                    <div className="flex flex-col">
+                        <div className="flex p-2 text-sm text-gray-700 bg-orange-100 rounded-lg  
+                                            justify-between"  role="alert">
+                            <h1 className="text-lg font-bold">Boards</h1>
+                            <button className='monochrome-purple-btn  flex space-x-2'>
+                                <FaPlusCircle className='w-4 h-4'></FaPlusCircle>
+                                新增
+                            </button>
+                        </div>
+                        <div className="overflow-x-auto ">
+                            <table className="table w-full ">
+                                <thead className=''>
+                                    <tr>
+                                        <th className='bg-green-200 w-16'></th>
+                                        <th className='bg-green-200'>Name</th>
+                                        <th className='bg-green-200'>Url</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        webPageData && webPageData.map((f: any) => <tr key={f.key}>
+                                            <th className='w-16	'>
+                                                <div className="flex flex-l">
+                                                    <div className="tooltip" data-tip="刪除">
+                                                        <button className='pill-red-btn' onClick={() => { 
+                                                            
+                                                        }}><FiTrash2></FiTrash2></button>
+                                                    </div>
+                                                </div>
+                                            </th>
+                                            <th>{f.Name}</th>
+                                            <th>{f.Url}</th>
+                                        </tr>)
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-row items-center  justify-center space-x-2">
+                        <button type="submit" className="btn btn-success">送出</button>
+                    </div>
 
                 </form>
             </div>
