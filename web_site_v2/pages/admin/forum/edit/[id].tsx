fa6fd66e95c580ff1d7b0eb4c649e5dac4e9df96
ben <moon791017@gmail.com>
2022-06-17 15:24:34 +0800

    test

--- web_site_v2/pages/admin/forum/edit/[id].tsx
+++ web_site_v2/pages/admin/forum/edit/[id].tsx
@@ -10,6 +10,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 import { FaPlusCircle } from "react-icons/fa";
 import { FiTrash2 } from "react-icons/fi";
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider, useDrag } from "react-dnd";
 const schema = yup.object({
     name: yup.string().required(),
     workerName: yup.number().required(),
@@ -67,42 +69,44 @@ const Edit = () => {
                     </label>
 
                     <div className="flex flex-col">
-                        <div className="flex p-2 text-sm text-gray-700 bg-orange-100 rounded-lg  
+                        {/* <DndProvider backend={HTML5Backend}> */}
+                            <div className="flex p-2 text-sm text-gray-700 bg-orange-100 rounded-lg  
                                             justify-between"  role="alert">
-                            <h1 className="text-lg font-bold">Boards</h1>
-                            <button className='monochrome-purple-btn  flex space-x-2'>
-                                <FaPlusCircle className='w-4 h-4'></FaPlusCircle>
-                                新增
-                            </button>
-                        </div>
-                        <div className="overflow-x-auto ">
-                            <table className="table w-full ">
-                                <thead className=''>
-                                    <tr>
-                                        <th className='bg-green-200 w-16'></th>
-                                        <th className='bg-green-200'>Name</th>
-                                        <th className='bg-green-200'>Url</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        webPageData && webPageData.map((f: any) => 
-                                        <tr key={f.key}>
-                                            <th className='w-16	'>
-                                                <div className="flex flex-l">
-                                                    <div className="tooltip" data-tip="刪除">
-                                                        <button className='pill-red-btn' onClick={() => {                                                            
-                                                        }}><FiTrash2></FiTrash2></button>
-                                                    </div>
-                                                </div>
-                                            </th>
-                                            <th>{f.Name}</th>
-                                            <th>{f.Url}</th>
-                                        </tr>)
-                                    }
-                                </tbody>
-                            </table>
-                        </div>
+                                <h1 className="text-lg font-bold">Boards</h1>
+                                <button className='monochrome-purple-btn  flex space-x-2'>
+                                    <FaPlusCircle className='w-4 h-4'></FaPlusCircle>
+                                    新增
+                                </button>
+                            </div>
+                            <div className="overflow-x-auto ">
+                                <table className="table w-full ">
+                                    <thead className=''>
+                                        <tr>
+                                            <th className='bg-green-200 w-16'></th>
+                                            <th className='bg-green-200'>Name</th>
+                                            <th className='bg-green-200'>Url</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {
+                                            webPageData && webPageData.map((f: any) =>
+                                                <tr key={f.key} role="Handle">
+                                                    <th className='w-16	'>
+                                                        <div className="flex flex-l">
+                                                            <div className="tooltip" data-tip="刪除">
+                                                                <button className='pill-red-btn' onClick={() => {
+                                                                }}><FiTrash2></FiTrash2></button>
+                                                            </div>
+                                                        </div>
+                                                    </th>
+                                                    <th>{f.Name}</th>
+                                                    <th>{f.Url}</th>
+                                                </tr>)
+                                        }
+                                    </tbody>
+                                </table>
+                            </div>
+                        {/* </DndProvider> */}
                     </div>
 
                     <div className="flex flex-row items-center  justify-center space-x-2">
