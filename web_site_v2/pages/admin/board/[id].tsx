916a57e3aa75b6915b264af300c5e1b76a40f1b6
ben <moon791017@gmail.com>
2022-07-08 16:36:58 +0800

    aaa

--- web_site_v2/pages/admin/board/[id].tsx
+++ web_site_v2/pages/admin/board/[id].tsx
@@ -1,15 +1,18 @@
+import { useRouter } from "next/router";
 import { ReactElement } from "react";
 import useSWR from "swr";
 import { adminGlobalStore, defaultAdminGlobalStoreData } from "../../../stores/admin-global-store";
 import AdminLayout from "../../utils/admin-layout";
 
 const Index = () => {
+    const router = useRouter();
+    const { id } = router.query;
     const { data: adminGlobalStoreData, mutate: adminGlobalStoreMutate } = useSWR(adminGlobalStore,
         { fallbackData: defaultAdminGlobalStoreData })
     adminGlobalStoreMutate({ ...defaultAdminGlobalStoreData, pageTitle: '看版管理', pageDescription: '管理看版裡的文章和圖片' }, false)
     return (
         <div>
-            Enter
+            {id}
         </div>
     );
 }
