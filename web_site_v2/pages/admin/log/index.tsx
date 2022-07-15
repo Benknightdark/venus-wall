295d8a8e1c935ca4642f68a64be62837cbc3f170
ben <moon791017@gmail.com>
2022-07-15 17:37:02 +0800

    test

---
+++ web_site_v2/pages/admin/log/index.tsx
@@ -0,0 +1,53 @@
+import { ReactElement, useEffect } from "react";
+import useSWR from "swr";
+import { adminGlobalStore, defaultAdminGlobalStoreData } from "../../../stores/admin-global-store";
+import AdminLayout from "../../utils/admin-layout";
+
+const Index = () => {
+    const { data: adminGlobalStoreData, mutate: adminGlobalStoreMutate } = useSWR(adminGlobalStore,
+        { fallbackData: defaultAdminGlobalStoreData })
+    useEffect(() => {
+        adminGlobalStoreMutate({ ...defaultAdminGlobalStoreData, pageTitle: '爬蟲執行記錄查詢', pageDescription: '查詢爬蟲的執行記錄' }, false)
+    })
+    return (
+        <div className="flex flex-col md:flex-row md:flex-wrap  space-x-3 justify-center">
+            <div className="card  bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">Card title!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">Card title!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card  bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">Card title!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+Index.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <AdminLayout>
+            {page}
+        </AdminLayout>
+    )
+}
+export default Index;
