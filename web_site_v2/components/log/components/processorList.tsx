6fd88bfaeec758d8149841a7be03ab5d80a0000b
ben <moon791017@gmail.com>
2022-07-21 16:47:09 +0800

    add createdatetime to log list component

--- web_site_v2/components/log/components/processorList.tsx
+++ web_site_v2/components/log/components/processorList.tsx
@@ -1,3 +1,5 @@
+import { FcOvertime } from "react-icons/fc";
+
 export const ProcessorList = (props: any) => {
     return <li
         onClick={() => {
@@ -6,6 +8,11 @@ export const ProcessorList = (props: any) => {
         className='animate__animated animate__flipInX animate__delay-1s
 border-b-4 border-gray-500
 ' >
-        <a><span className="text-red-600">【{props.w.ForumName}/{props.w.WebPageName} - {props.w.Title}】</span> 已寫入資料庫</a>
+        <a className="flex flex-col justify-start items-start">
+            <div><span className="text-red-600">【{props.w.ForumName}/{props.w.WebPageName} - {props.w.Title}】</span> 已寫入資料庫</div>
+            <div className='flex flex-row  space-x-2'>
+                <FcOvertime className='w-5 h-5'></FcOvertime><span className='text-gray-400'>{props.w.CreateDateTime.toString().split('.')[0]}</span>
+            </div>
+        </a>
     </li>;
 }
--- web_site_v2/components/log/components/workerList.tsx
+++ web_site_v2/components/log/components/workerList.tsx
@@ -1,5 +1,13 @@
+import { FcOvertime } from "react-icons/fc"
+
 export const WorkerList = (props: any) => {
     return <li
-        className="border-b-4 border-blue-500" ><a>{props.w.WebPageName}</a>
+        className="border-b-4 border-blue-500" >
+            <a className="flex flex-col justify-start items-start">
+            {props.w.WebPageName}
+            <div className='flex flex-row space-x-2'>
+                <FcOvertime className='w-5 h-5'></FcOvertime><span className='text-gray-400'>{props.w.CreateDateTime.toString().split('.')[0]}</span>
+            </div>
+        </a>
     </li>
 }
