aac3fe03f36b5dab8d1968bc2c618f896a48dd2f
ben <moon791017@gmail.com>
2022-08-02 10:28:38 +0800

    OK

--- web_site_v2/pages/_app.tsx
+++ web_site_v2/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import useSWR, { SWRConfig } from 'swr'
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -17,8 +18,15 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return getLayout(
-
+    <SWRConfig
+      value={{
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false
+      }}
+    >
       <Component {...pageProps} />
+    </SWRConfig>
 
   )
 }
